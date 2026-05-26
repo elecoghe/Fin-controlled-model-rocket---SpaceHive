1f85eb3cf70e675d340489fa48a03746717ffb5c
Elena Coghe <116958418+elecoghe@users.noreply.github.com>
2026-05-26 10:17:49 +0200

    Update discretisation_closed_loop_function.m
    Updated the discretisation function to include comments on the Tustin discretisation approach and sampling time.

--- SpaceHive fin control/Simulation/discretisation_closed_loop_function.m
+++ SpaceHive fin control/Simulation/discretisation_closed_loop_function.m
@@ -1,7 +1,7 @@
 s = tf('s');
 G = 1/s^2;
 
-G_disc = c2d(G, 20e-3, 'tustin');
+G_disc = c2d(G, 20e-3, 'tustin'); %%Tustin discretisation approach, sampling time 20 ms (due to DC brushless dynamics)
 L = G_disc/(1+G_disc);
 
 step(L);
